fa411476c26b994f1fbb7fe7051bc255bbb722cc
Amayakite <amayakite@qq.com>
2022-07-14 11:04:31 +0800

    add  全局后置钩子影响document.title

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,5 +7,16 @@ const router = createRouter({
     // 路由的详细配置
     routes: route
 });
+// 全局后置钩子
+router.afterEach((to, from) => {
+    // 获取from的meta.title
+    const fromTitle: string = to.meta.title as string;
+    // 设置到title
+    document.title = fromTitle
+        ? fromTitle
+        : (from.meta.title as string)
+        ? (from.meta.title as string)
+        : document.title;
+});
 
 export default router;
