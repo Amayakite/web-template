28fd1d991a3ae6e6c6e07e57fc7eea203cd27f7d
Amayakite <amayakite@qq.com>
2022-08-04 14:46:36 +0800

    update obj deep clone

--- src/util/objects.ts
+++ src/util/objects.ts
@@ -20,12 +20,13 @@ export default class ObjectsUtil {
     /**
      * 深拷贝对象
      * @param obj 要拷贝的对象
+     * @param isDeep? 是否深拷贝 默认否，是的话则会拷贝原型链上的属性
      * @returns  拷贝后的对象
      * @example
      * const obj = {a: 1, b: {c: 2}} // obj
      * const copy = deepCopy(obj) // {a: 1, b: {c: 2}}
      */
-    static deepCopy<T>(obj: T): T {
+    static deepCopy<T>(obj: T, isDeep = true): T {
         // 如果是基础类型，直接返回
         if (typeof obj !== 'object') {
             return obj;
@@ -33,14 +34,27 @@ export default class ObjectsUtil {
         // 如果是数组，则返回一个新数组
         if (Array.isArray(obj)) {
             // @ts-ignore
-            return obj.map((item) => this.deepCopy(item));
+            return obj.map((item) => this.deepCopy(item, isDeep));
         }
-        // 如果是对象，则返回一个新对象
-        const newObj = {} as T;
+        if (!isDeep) {
+            const newObj = {} as T;
+            for (const key in obj) {
+                // @ts-ignore
+                if (obj.hasOwnProperty(key)) {
+                    newObj[key] = this.deepCopy(obj[key], isDeep);
+                }
+            }
+            return newObj;
+        }
+        // 获取对象的原型链
+        const proto = Object.getPrototypeOf(obj);
+        // 创建一个新对象
+        const newObj = Object.create(proto);
+        // 遍历对象的属性
         for (const key in obj) {
             // @ts-ignore
             if (obj.hasOwnProperty(key)) {
-                newObj[key] = this.deepCopy(obj[key]);
+                newObj[key] = this.deepCopy(obj[key], isDeep);
             }
         }
         return newObj;
